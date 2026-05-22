e6e8caffcec4610ef7b2d0055eb278eb25d1e38d
domixde21 <drakeallendominick@gmail.com>
2026-05-21 22:44:34 -0500

    fronted estructurado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-700">
+        SGA Leguía funcionando
+      </h1>
+    </div>
+  );
+}
+
+export default App;
